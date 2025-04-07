6a0909578d1a87cecd2ec5928d063fe9e64ad9dd
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-04-07 10:47:51 +0200

    ajout du style

---
+++ script.js
@@ -0,0 +1,32 @@
+const text_animate = document.querySelector('.second-text')
+
+const textLoad = () => {
+    setTimeout(() => {
+        text_animate.textContent = 'Développeuse Web';
+    }, 0);
+    setTimeout(() => {
+        text_animate.textContent = 'Full-stack';
+    }, 4000);
+    setTimeout(() => {
+        text_animate.textContent = 'En reconversion professionnelle';
+    }, 8000);
+    setTimeout(() => {
+        text_animate.textContent = 'Cool et ouverte d\'esprit';
+    }, 12000);
+    setTimeout(() => {
+        text_animate.textContent = 'Désireuse d\'en apprendre davantage';
+    }, 16000);
+    setTimeout(() => {
+        text_animate.textContent = 'À la recherche d\'une alternance';
+    }, 20000);
+}
+textLoad()
+setInterval(textLoad, 12000);
+
+
+// Menu déroulant
+let menuIcon = document.getElementById('menu');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
